9604719da878c4486b3b21175eaacdc5cd0d6847
kerry <zihao317@gmail.com>
2016-06-04 10:28:28 +0800

    [封裝]增加Operation運算類別

--- Computer/Program.cs
+++ Computer/Program.cs
@@ -11,42 +11,27 @@ namespace Computer
         /// <summary>
         /// 1. 完成簡易版計算機
         /// 2. 重新命名變數，改用switch判斷strOperate，增加判斷除數不能為0
+        /// 3. [封裝]增加Operation運算類別
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.Write("請輸入數字：");
-            string strNumberA = Console.ReadLine();
-            Console.Write("請輸入(+ - * / )：");
-            string strOperate = Console.ReadLine();
-            Console.Write("請輸入數字：");
-            string strNumberB = Console.ReadLine();
-            string strResult = string.Empty;
-
-            switch (strOperate)
+            try
             {
-                case "+":
-                    strResult = Convert.ToString(Convert.ToInt16(strNumberA) + Convert.ToInt16(strNumberB));
-                    break;
-                case "-":
-                    strResult = Convert.ToString(Convert.ToInt16(strNumberA) - Convert.ToInt16(strNumberB));
-                    break;
-                case "*":
-                    strResult = Convert.ToString(Convert.ToInt16(strNumberA) * Convert.ToInt16(strNumberB));
-                    break;
-                case "/":
-                    if (!strNumberB.Equals("0"))
-                    {
-                        strResult = Convert.ToString(Convert.ToInt16(strNumberA) / Convert.ToInt16(strNumberB));
-                    }
-                    else
-                    {
-                        strResult = "除數不能為0";
-                    }
-                    break;
+                Console.Write("請輸入數字：");
+                string strNumberA = Console.ReadLine();
+                Console.Write("請輸入(+ - * / )：");
+                string strOperate = Console.ReadLine();
+                Console.Write("請輸入數字：");
+                string strNumberB = Console.ReadLine();
+                string strResult = Convert.ToString(Operation.GetResult(Convert.ToDouble(strNumberA), Convert.ToDouble(strNumberB), strOperate));                
+                Console.WriteLine("結果：" + strResult);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("結果：" + ex.Message);
             }            
-            Console.WriteLine("結果：" + strResult);
-            Console.ReadKey();
         }
     }
 }
