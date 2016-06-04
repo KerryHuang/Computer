14b01b03a84fed1b1614a025d348868bec55c9b9
kerry <zihao317@gmail.com>
2016-06-04 09:33:08 +0800

    計算機-IF判斷

---
+++ Computer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入數字：");
+            string A = Console.ReadLine();
+            Console.Write("請輸入(+ - * / )：");
+            string B = Console.ReadLine();
+            Console.Write("請輸入數字：");
+            string C = Console.ReadLine();
+            string D = string.Empty;
+
+            if (B.Equals("+"))
+            {
+                D = Convert.ToString(Convert.ToInt16(A) + Convert.ToInt16(C));
+            }
+            if (B.Equals("-"))
+            {
+                D = Convert.ToString(Convert.ToInt16(A) - Convert.ToInt16(C));
+            }
+            if (B.Equals("*"))
+            {
+                D = Convert.ToString(Convert.ToInt16(A) * Convert.ToInt16(C));
+            }
+            if (B.Equals("/"))
+            {
+                D = Convert.ToString(Convert.ToInt16(A) / Convert.ToInt16(C));
+            }
+            Console.WriteLine("結果：" + D);
+            Console.ReadKey();
+        }
+    }
+}
